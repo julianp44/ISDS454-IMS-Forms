6a4d36487b1cf9789e7871c94f702408591fe2b1
julianp44 <75781251+julianp44@users.noreply.github.com>
2024-11-08 15:47:51 -0800

    -Created Delete Item Confirmation Page -added datagridview table to display selected item details -added cancel button (functional) -Added Confirm Delete button (non-functional)

---
+++ DeleteItemConfirmationForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ISDS454_IMS_Forms
+{
+    partial class DeleteItemConfirmationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ConfirmDeleteLabel = new Label();
+            DeleteItemDataGrid = new DataGridView();
+            ConfirmDeleteButton = new Button();
+            DeleteCancelButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)DeleteItemDataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // ConfirmDeleteLabel
+            // 
+            ConfirmDeleteLabel.AutoSize = true;
+            ConfirmDeleteLabel.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ConfirmDeleteLabel.Location = new Point(235, 9);
+            ConfirmDeleteLabel.Name = "ConfirmDeleteLabel";
+            ConfirmDeleteLabel.Size = new Size(338, 45);
+            ConfirmDeleteLabel.TabIndex = 0;
+            ConfirmDeleteLabel.Text = "Confirm Delete Item?";
+            // 
+            // DeleteItemDataGrid
+            // 
+            DeleteItemDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DeleteItemDataGrid.Location = new Point(12, 69);
+            DeleteItemDataGrid.Name = "DeleteItemDataGrid";
+            DeleteItemDataGrid.Size = new Size(776, 298);
+            DeleteItemDataGrid.TabIndex = 1;
+            // 
+            // ConfirmDeleteButton
+            // 
+            ConfirmDeleteButton.BackColor = Color.FromArgb(255, 128, 128);
+            ConfirmDeleteButton.Location = new Point(667, 394);
+            ConfirmDeleteButton.Name = "ConfirmDeleteButton";
+            ConfirmDeleteButton.Size = new Size(121, 44);
+            ConfirmDeleteButton.TabIndex = 2;
+            ConfirmDeleteButton.Text = "Confirm Delete";
+            ConfirmDeleteButton.UseVisualStyleBackColor = false;
+            ConfirmDeleteButton.Click += ConfirmDeleteButton_Click;
+            // 
+            // DeleteCancelButton
+            // 
+            DeleteCancelButton.BackColor = SystemColors.Control;
+            DeleteCancelButton.Location = new Point(540, 394);
+            DeleteCancelButton.Name = "DeleteCancelButton";
+            DeleteCancelButton.Size = new Size(121, 44);
+            DeleteCancelButton.TabIndex = 3;
+            DeleteCancelButton.Text = "Cancel";
+            DeleteCancelButton.UseVisualStyleBackColor = false;
+            DeleteCancelButton.Click += DeleteCancelButton_Click;
+            // 
+            // DeleteItemConfirmationForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(DeleteCancelButton);
+            Controls.Add(ConfirmDeleteButton);
+            Controls.Add(DeleteItemDataGrid);
+            Controls.Add(ConfirmDeleteLabel);
+            Name = "DeleteItemConfirmationForm";
+            Text = "Item Delete Confirmation";
+            ((System.ComponentModel.ISupportInitialize)DeleteItemDataGrid).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label ConfirmDeleteLabel;
+        private DataGridView DeleteItemDataGrid;
+        private Button ConfirmDeleteButton;
+        private Button DeleteCancelButton;
+    }
+}
---
+++ DeleteItemConfirmationForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISDS454_IMS_Forms
+{
+    public partial class DeleteItemConfirmationForm : Form
+    {
+        public DeleteItemConfirmationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteCancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); //Closes this window
+        }
+
+        private void ConfirmDeleteButton_Click(object sender, EventArgs e)
+        {
+            //needs code to delete selected item from sql database
+            //needs code to open Delete Success page upon successfull delete
+        }
+    }
+}
