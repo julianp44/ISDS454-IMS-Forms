6ac92e574352e5a4eeb901013fcee2422a91222d
julianp44 <75781251+julianp44@users.noreply.github.com>
2024-11-11 22:29:16 -0800

    -addressed issue of scaling when resizing window, all contents are now properly anchored/unanchored. -datagridview autosize columns set to all cells -datagridview autosize rows set to all cells

--- EditInventoryMain.Designer.cs
+++ EditInventoryMain.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             EditIMSLabel = new Label();
             AddItemButton = new Button();
             EditInventoryCancelButton = new Button();
@@ -48,10 +46,11 @@
             // 
             // EditIMSLabel
             // 
+            EditIMSLabel.Anchor = AnchorStyles.Top;
             EditIMSLabel.AutoSize = true;
             EditIMSLabel.ForeColor = SystemColors.ControlText;
             EditIMSLabel.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Polite;
-            EditIMSLabel.Location = new Point(653, 58);
+            EditIMSLabel.Location = new Point(276, 58);
             EditIMSLabel.Margin = new Padding(8, 0, 8, 0);
             EditIMSLabel.Name = "EditIMSLabel";
             EditIMSLabel.Size = new Size(563, 45);
@@ -60,8 +59,9 @@
             // 
             // AddItemButton
             // 
+            AddItemButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             AddItemButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            AddItemButton.Location = new Point(12, 998);
+            AddItemButton.Location = new Point(12, 447);
             AddItemButton.Name = "AddItemButton";
             AddItemButton.Size = new Size(161, 35);
             AddItemButton.TabIndex = 2;
@@ -70,9 +70,10 @@
             // 
             // EditInventoryCancelButton
             // 
+            EditInventoryCancelButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             EditInventoryCancelButton.BackColor = Color.FromArgb(255, 128, 128);
             EditInventoryCancelButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            EditInventoryCancelButton.Location = new Point(12, 1096);
+            EditInventoryCancelButton.Location = new Point(12, 545);
             EditInventoryCancelButton.Name = "EditInventoryCancelButton";
             EditInventoryCancelButton.Size = new Size(180, 47);
             EditInventoryCancelButton.TabIndex = 3;
@@ -82,9 +83,10 @@
             // 
             // NextButton
             // 
+            NextButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             NextButton.BackColor = Color.CornflowerBlue;
             NextButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            NextButton.Location = new Point(1633, 1096);
+            NextButton.Location = new Point(879, 545);
             NextButton.Name = "NextButton";
             NextButton.Size = new Size(180, 47);
             NextButton.TabIndex = 4;
@@ -94,9 +96,10 @@
             // 
             // UpdateItemCheck
             // 
+            UpdateItemCheck.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             UpdateItemCheck.AutoSize = true;
             UpdateItemCheck.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            UpdateItemCheck.Location = new Point(196, 1008);
+            UpdateItemCheck.Location = new Point(196, 457);
             UpdateItemCheck.Name = "UpdateItemCheck";
             UpdateItemCheck.Size = new Size(114, 25);
             UpdateItemCheck.TabIndex = 5;
@@ -105,9 +108,10 @@
             // 
             // DeleteItemCheck
             // 
+            DeleteItemCheck.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             DeleteItemCheck.AutoSize = true;
             DeleteItemCheck.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            DeleteItemCheck.Location = new Point(327, 1008);
+            DeleteItemCheck.Location = new Point(327, 457);
             DeleteItemCheck.Name = "DeleteItemCheck";
             DeleteItemCheck.Size = new Size(108, 25);
             DeleteItemCheck.TabIndex = 6;
@@ -116,27 +120,14 @@
             // 
             // EditInventoryDataTable
             // 
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            EditInventoryDataTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            EditInventoryDataTable.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            EditInventoryDataTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            EditInventoryDataTable.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             EditInventoryDataTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             EditInventoryDataTable.Columns.AddRange(new DataGridViewColumn[] { InventorySKU, ItemName, ItemQuantity, ItemLocation });
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = SystemColors.Window;
-            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
-            EditInventoryDataTable.DefaultCellStyle = dataGridViewCellStyle2;
             EditInventoryDataTable.Location = new Point(12, 174);
             EditInventoryDataTable.Name = "EditInventoryDataTable";
-            EditInventoryDataTable.Size = new Size(1801, 807);
+            EditInventoryDataTable.Size = new Size(1047, 256);
             EditInventoryDataTable.TabIndex = 7;
             // 
             // InventorySKU
@@ -165,9 +156,10 @@
             // 
             // EditSearchLabel
             // 
+            EditSearchLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             EditSearchLabel.AutoSize = true;
             EditSearchLabel.Font = new Font("Segoe UI", 9.75F);
-            EditSearchLabel.Location = new Point(1543, 146);
+            EditSearchLabel.Location = new Point(789, 146);
             EditSearchLabel.Name = "EditSearchLabel";
             EditSearchLabel.Size = new Size(50, 17);
             EditSearchLabel.TabIndex = 10;
@@ -175,8 +167,9 @@
             // 
             // EditSearchTextBox
             // 
+            EditSearchTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             EditSearchTextBox.Font = new Font("Segoe UI", 9.75F);
-            EditSearchTextBox.Location = new Point(1594, 143);
+            EditSearchTextBox.Location = new Point(840, 143);
             EditSearchTextBox.Name = "EditSearchTextBox";
             EditSearchTextBox.Size = new Size(219, 25);
             EditSearchTextBox.TabIndex = 9;
@@ -187,7 +180,7 @@
             AutoScaleDimensions = new SizeF(19F, 45F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(1825, 1155);
+            ClientSize = new Size(1071, 604);
             Controls.Add(EditSearchLabel);
             Controls.Add(EditSearchTextBox);
             Controls.Add(EditInventoryDataTable);
